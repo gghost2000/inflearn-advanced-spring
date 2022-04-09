172a6cad350b6b409d63cb14244b2dbbeff6f181
jihwan.hyun <gghost2000@naver.com>
2022-04-09 23:06:39 +0900

    필드동기화

---
+++ src/main/java/hello/advanced/app/v3/OrderRepositoryV3.java
@@ -0,0 +1,43 @@
+package hello.advanced.app.v3;
+
+
+import hello.advanced.trace.TraceId;
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV3 {
+
+    private final LogTrace trace;
+
+    public void save(String itemId) {
+
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderRepositoryV.save()");
+
+            if (itemId.equals("ex")) {
+                throw new IllegalStateException("예외 발생!");
+            }
+
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+
+        sleep(1000);
+    }
+
+    private void sleep(int milils) {
+        try {
+            Thread.sleep(milils);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
